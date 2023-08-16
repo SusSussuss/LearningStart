b158bf24d0bc37b9d6be28b4b273fc1dd373587f
Alexander.Ponomarenko <alexvp2807@gmail.com>
2023-08-16 22:18:17 +0300

    Add solution

---
+++ FirstSolution/FirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyProgram
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bgdfbgfgb");
+            Console.ReadLine();
+        }
+    }
+}
