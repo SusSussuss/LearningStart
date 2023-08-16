46ec14683fcd8d2ccf9d48c67caf7cfd9b604558
Alexander.Ponomarenko <alexvp2807@gmail.com>
2023-08-16 22:23:41 +0300

    Change line2

--- FirstSolution/FirstConsoleApp/Program.cs
+++ FirstSolution/FirstConsoleApp/Program.cs
@@ -5,7 +5,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("bgdfb");
+            Console.WriteLine("bgderfgergfb");
             Console.ReadLine();
         }
     }
