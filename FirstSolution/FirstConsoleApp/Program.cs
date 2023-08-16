98de492cab43b14fdbad4a51aed3ad6137411835
Alexander.Ponomarenko <alexvp2807@gmail.com>
2023-08-16 22:28:19 +0300

    Change6

--- FirstSolution/FirstConsoleApp/Program.cs
+++ FirstSolution/FirstConsoleApp/Program.cs
@@ -5,7 +5,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("bgergfffffffffffffffffffffffffhdddergdfb");
+            Console.WriteLine("bgerghdffffddergdfb");
             Console.ReadLine();
         }
     }
